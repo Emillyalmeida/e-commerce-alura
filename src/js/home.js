baa365b1e4260025a39c19857631218572e2f9e2
Emillyalmeida <emillyalmeidan@gmail.com>
2023-02-23 21:09:55 -0300

    js: create card products dinamic with db

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,63 @@
 import validty from "./validty.js";
+import listProducts from "./db.js";
 
 const btnLogin = document.querySelector(".header__button");
+
+const containerProduct = document.querySelector(".products");
+const categories = [];
+
+const listCategories = () => {
+  listProducts.forEach(product=> {
+    if(!categories.includes(product.category)){
+      categories.push(product.category)
+    }
+  })
+};
+
+
+const createCardProduct = ( ulProducts,category) => {
+  listProducts.forEach(product=> {
+
+    if(product.category == category){
+      const liCard = document.createElement("li");
+      liCard.classList.add("products__card")
+      liCard.innerHTML = 
+      `
+        <img
+          class="products__card-img"
+          src=${product.img}
+          alt="${product.name}"
+        />
+        <h3 class="products__card-name">${product.name}</h3>
+        <span class="products__card-price">R$ ${product.price.toFixed(2).replace('.',',')}</span>
+        <a class="products__card-link">ver produto</a>
+      `
+
+      ulProducts.appendChild(liCard)
+    }
+
+  })
+};
+
+const createDivCategory = () => {
+  categories.forEach(category => {
+    const divProduct = document.createElement("div");
+    divProduct.classList.add("products__category");
+
+    const ulProducts = document.createElement("ul");
+    ulProducts.classList.add("products__list-card");
+
+    createCardProduct(ulProducts,category)
+
+    divProduct.innerHTML = `<h2 class="products__title">${category}</h2>`
+    divProduct.appendChild(ulProducts)
+
+    containerProduct.appendChild(divProduct)
+  })
+};
+
+listCategories();
+createDivCategory();
 
 btnLogin.addEventListener("click", () => {
   window.location.href = "./src/pages/login.html";
